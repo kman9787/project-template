eb0b7ed5245487de0a087313a82670a3e078e3fc
kazancheta87@gmail.com <sd01849sd01849>
2019-09-28 22:24:09 +0800

    initial version

---
+++ genericweb/src/test/java/org/kash/template/test/Test1.java
@@ -0,0 +1,14 @@
+package org.kash.template.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test1 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
