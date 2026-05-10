f8f276ee83f9a0dc11b988192bcb2c30c37dd506
kman9787 <kazancheta87@gmail.com>
2026-05-11 01:02:38 +0800

    Change database password to 'appdevpass'
    Updated database password in deployment script and configuration files.

--- scripts/deploy_todo.sh
+++ scripts/deploy_todo.sh
@@ -6,11 +6,11 @@ source ./replace_prop.sh
 rm -rf $APP_DIR
 git clone https://github.com/kman9787/todo-app.git $APP_DIR
 # Prepare local config 
-replace_prop "flyway.password" "sd01849sd01849" $CONF_DIR/flyway.conf
+replace_prop "flyway.password" "appdevpass" $CONF_DIR/flyway.conf
 replace_prop "MYSQL_DATABASE" "todo_db" $APP_DIR/.env
 replace_prop "MYSQL_USER" "todo_db_user" $APP_DIR/.env
-echo "sd01849sd01849" > $CONF_DIR/db_psswd.txt
-echo "sd01849sd01849" > $CONF_DIR/db_root_psswd.txt
+echo "appdevpass" > $CONF_DIR/db_psswd.txt
+echo "appdevpass" > $CONF_DIR/db_root_psswd.txt
 # Run the application deployment script
 cd $APP_DIR
 exec ./run.sh
